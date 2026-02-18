a407fca699211503ed84c41ca374c49d856eddb0
SamKanini <sam.kanini18@gmail.com>
2026-02-18 06:31:18 +0300

    Register page loads first

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,32 @@
-import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
 import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
-import ProtectedRoute from "./components/ProtectedRoutes";
-import { ACCESS_TOKEN } from "./constants";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function Logout() {
-  localStorage.clear();
-  return <Navigate to={"/login"} />;
-}
-
-function RegisterAndLogout() {
-  localStorage.clear();
-  return <Register />;
-}
 function App() {
-  const isAuthenticated = !!localStorage.getItem(ACCESS_TOKEN);
-
   return (
     <BrowserRouter>
       <Routes>
-        {/* check if authenticated go to home if not go to register */}
-        <Route
-          path="/"
-          element={
-            isAuthenticated ? (
-              <Navigate to="/home" replace />
-            ) : (
-              <Navigate to="/register" replace />
-            )
-          }
-        />
-        <Route path="/home" element={<Home />} />
+        {/* Default route redirects to register */}
+        <Route path="/" element={<Navigate to="/register" replace />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route
-          path="/"
+          path="/home"
           element={
             <ProtectedRoute>
               <Home />
             </ProtectedRoute>
           }
         />
-
-        <Route path="/logout" element={<Logout />} />
+        <Route path="/logout" element={<Logout />} />;
         {/* <Route path="/register" element={<RegisterAndLogout />} /> */}
-        <Route path="*" element={<NotFound />}></Route>
+        <Route path="*" element={<NotFound />}></Route>;
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
